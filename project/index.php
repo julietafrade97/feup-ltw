33a1406b4cfa48f52f2d1fd5fed164ab145ec9cd
Sofia Silva <up201505628@fe.up.pt>
2017-11-23 00:38:22 +0000

    started coding :bug:
    header done
    added icon library (two actually)

---
+++ project/index.php
@@ -0,0 +1,3 @@
+<?php
+  include_once('templates/common/header.php');
+?>
---
+++ project/templates/common/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Title</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/linearicons/Web Font/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+    <header>
+        <div id="info">
+            <a href="index.php">
+                <img src="assets/logo.svg">
+            </a>
+            <h1>
+                <a href="index.php">Elysian</a>
+            </h1>
+            <span class="lnr lnr-magnifier"></span>
+            <span class="lnr lnr-cog"></span>
+        </div>
+    </header>
