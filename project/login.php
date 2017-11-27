695a926027bb699da4adf81fdc1266e150af0ec0
Sofia Silva <up201505628@fe.up.pt>
2017-11-27 11:29:34 +0000

    login :cherries:

--- project/login.php
+++ project/login.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Elysian</title>
+    <title>Meraki</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
@@ -15,10 +15,14 @@
 <body>
     <div id="login_container">
         <div id="login_header">
+            <a href="sign_up.php" class="signup_button">Sign Up</a>
             <img src="assets/logo.svg">
-            <h1>elysian</h1>
+            <h1>Meraki</h1>
         </div>
         <div id="login_content">
-            <a href="sign_up.php" class="button">Sign Up</a>
+            <h1>Login</h1>
+            <input name="username" class="w3-input w3-border" type="text" placeholder="Username">
+            <input name="password" class="w3-input w3-border" type="password" placeholder="Password">
+            <a href="#" class="default_button">Next</a>
             <a href="test.php" class="button">Test Data Base</a>
         </div>
