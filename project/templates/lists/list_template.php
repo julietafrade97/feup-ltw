249a762350de100d206499ff1a2e27012e0a4be1
julietafrade97 <up201506530@fe.up.pt>
2017-11-28 08:49:16 +0000

    inicio do template de listas

---
+++ project/templates/lists/list_template.php
@@ -0,0 +1,15 @@
+<main>
+    <div id="list_container">
+            <div id="list_header">
+                <h1> Title </h1>
+            </div>
+            <div id="list_content">
+                <ul>
+                    <li> Task </li>
+                    <li> Task </li>
+                    <li> Task </li>
+                    <li> Task </li>
+                </ul>
+            </div>
+    </div>
+</main>
