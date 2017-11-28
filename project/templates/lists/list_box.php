8ec8fcff3cb00afd839f47a4097e66058e7be4ba
julietafrade97 <up201506530@fe.up.pt>
2017-11-28 10:24:50 +0000

    list flex e checkbox

---
+++ project/templates/lists/list_box.php
@@ -0,0 +1,51 @@
+<div id="list_box">
+    <h1> Title </h1>
+    <div id="task_list">
+        <input type="checkbox" id="box-1"> <label for="box-1">Task</label>
+        <input type="checkbox" id="box-2"> <label for="box-2">Task</label>
+        <input type="checkbox" id="box-3"> <label for="box-3">Task</label>
+        <input type="checkbox" id="box-4"> <label for="box-4">Task</label>
+    </div>
+</div>
+
+<div id="list_box">
+    <h1> Title </h1>
+    <div id="task_list">
+        <ul>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+        </ul>
+        <p>...</p>
+    </div>
+</div>
+
+<div id="list_box">
+    <h1> Title </h1>
+    <div id="task_list">
+        <ul>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+        </ul>
+        <p>...</p>
+    </div>
+</div>
+
+<div id="list_box">
+    <h1> Title </h1>
+    <div id="task_list">
+        <ul>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+            <li> <input type="checkbox"> Task </li>
+        </ul>
+        <p>...</p>
+    </div>
+</div>
