b6ee2db19e713eef08574bad4ed438b2c435a548
Sofia Silva <up201505628@fe.up.pt>
2017-11-29 13:29:37 +0000

    started single project

---
+++ project/templates/projects/single_project.php
@@ -0,0 +1,10 @@
+<div id="project_container">
+    <div id="bio">
+        <h1>Project</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porttitor eget quam non gravida. Proin quis risus
+            nisi. Phasellus eu purus lectus. In pharetra molestie.</p>
+    </div>
+    <div id="members">
+        
+    </div>
+</div>
