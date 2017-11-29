9662f2ae7a56e262b18c29fa5502d0ddedcb5bd9
Luís Martins <luis.n.martins97@gmail.com>
2017-11-29 13:06:30 +0000

    Update register user

--- project/includes/init.php
+++ project/includes/init.php
@@ -1,4 +1,12 @@
 <?php
   include_once('includes/session.php');
   include_once('database/connection.php');
+
+  if(isset($_SESSION['ERROR'])){
+  	$error = $_SESSION['ERROR'];
+  	unset($_SESSION['ERROR']);
+
+  } else {
+  	$error = "";
+  }
 ?>
--- project/sign_up.php
+++ project/sign_up.php
@@ -1,14 +1,7 @@
-<?php include_once('templates/common/header.php') ?>
-<script type="text/javascript" src="js/api_register.js"></script>
-<div id="signUpBox">
-	<form onsubmit="return submitRegisterAjax(event);" action="" method="post">
-		<input type="text" placeholder="write your username" id="username"><br>
-		<input type="password" placeholder="write your password" id="password"><br>
-		<input type="text" placeholder="write your name" id="name"><br>
-		<input type="email" placeholder="write your email" id="email"><br>
-		<input type="file" id="profilePhoto" accept="image/*"><br>
-		<input type="submit" id="Submit" value="signUp">
-	</form>
-	<p></p>
-</div>
-<?php include_once('templates/common/footer.php') ?>
+<?php
+
+include_once("includes/init.php");
+include_once("templates/user/sign_up.php");
+include_once("templates/common/footer.php");
+
+?>
