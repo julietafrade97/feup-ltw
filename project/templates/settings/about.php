58f15fbd77274f1b78ce23b220079244e2a2a2de
Sofia Silva <up201505628@fe.up.pt>
2017-12-02 20:01:10 +0000

    settings: archive and about :white_check_mark:

---
+++ project/templates/settings/about.php
@@ -0,0 +1,21 @@
+<div id="settings">
+    <h1>About</h1>
+    <div id="content">
+        <div id="about">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a purus dolor. Nullam fermentum tristique lobortis.
+                Cras ac aliquet libero. Phasellus cursus malesuada nunc. Proin est lacus, accumsan ut venenatis consequat,
+                bibendum quis nisl. Curabitur dolor leo, tempus vel diam sed, lacinia viverra nisl. Vestibulum hendrerit
+                justo in justo lacinia, ac rutrum dui commodo. Sed viverra, tortor in venenatis vehicula, tellus tortor aliquet
+                dui, nec facilisis libero ante sed est.
+            </p>
+            <p>
+                Ut lacinia semper mauris vel lacinia. Integer convallis scelerisque imperdiet. Class aptent taciti sociosqu ad litora torquent
+                per conubia nostra, per inceptos himenaeos. Fusce ultricies fermentum odio, in consectetur quam aliquet ut.
+                Mauris nec sagittis purus, sit amet iaculis nisl. Suspendisse dignissim, purus hendrerit euismod blandit,
+                ipsum quam tristique dolor, ut varius nisi dolor non justo. Duis quis nisi mattis, ultrices nunc nec, consectetur
+                nibh.
+            </p>
+            <p>Enjoy.</p>
+        </div>
+    </div>
+</div>
---
+++ project/templates/settings/archive.php
@@ -0,0 +1,34 @@
+<div id="settings">
+    <h1>Archive</h1>
+    <div id="content">
+        <div id="archive">
+            <div id="archived_lists">
+                <h2>Lists</h2>
+                <ul>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>List 1</li>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>List 2</li>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>List 3</li>
+                </ul>
+            </div>
+            <div id="archived_projects">
+                <h2>Projects</h2>
+                <ul>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>Project 1</li>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>Project 2</li>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>Project 3</li>
+                    <li>
+                        <span class="lnr lnr-chevron-right-circle"></span>Project 4</li>
+                </ul>
+            </div>
+            <div id="archive_buttons">
+                <input type="submit" name="Submit" value="Clear">
+            </div>
+        </div>
+    </div>
+</div>
