885b6de9d34652b830b47c3cc34a261ed6af85a7
julietafrade97 <up201506530@fe.up.pt>
2017-12-03 14:23:41 +0000

    base janela modal finalmente

--- project/templates/lists/list_box.php
+++ project/templates/lists/list_box.php
@@ -1,3 +1,13 @@
+<div id="listPopup" class="overlay">
+	<div class="popup">
+		<h2>Here i am</h2>
+		<a class="close" href="#">×</a>
+		<div class="content">
+			Thanks for pop me out of that button, but now i'm done so you can close this window.
+		</div>
+	</div>
+</div>
+
 <div id="list_box">
     <div id="list_box_header">
         <h1> Title </h1>
@@ -9,12 +19,7 @@
         <input type="checkbox" id="box-3"> <label for="box-3">Task</label>
         <input type="checkbox" id="box-4"> <label for="box-4">Task</label>
     </div>
-    <div id="more">
-        <p id="etc" >. . .</p>
-        <div id="hover_box">
-            <p> SEE MORE </p>
-        </div>
-    </div>
+    <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
 
 <div id="list_box">
@@ -26,8 +31,8 @@
         <input type="checkbox" id="box-6"> <label for="box-6">Task</label>
         <input type="checkbox" id="box-7"> <label for="box-7">Task</label>
         <input type="checkbox" id="box-8"> <label for="box-8">Task</label>
-        <p>. . .</p>
     </div>
+    <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
 
 <div id="list_box">
@@ -40,8 +45,8 @@
         <input type="checkbox" id="box-10"> <label for="box-10">Task</label>
         <input type="checkbox" id="box-11"> <label for="box-11">Task</label>
         <input type="checkbox" id="box-12"> <label for="box-12">Task</label>
-        <p>. . .</p>
     </div>
+    <a id="more" href="#listPopup" type="button" >SEE MORE</a>  
 </div>
 
 <div id="list_box">
@@ -54,8 +59,8 @@
         <input type="checkbox" id="box-14"> <label for="box-14">Task</label>
         <input type="checkbox" id="box-15"> <label for="box-15">Task</label>
         <input type="checkbox" id="box-16"> <label for="box-16">Task</label>
-        <p>. . .</p>
     </div>
+    <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
 
 
@@ -68,8 +73,8 @@
         <input type="checkbox" id="box-18"> <label for="box-18">Task</label>
         <input type="checkbox" id="box-19"> <label for="box-19">Task</label>
         <input type="checkbox" id="box-20"> <label for="box-20">Task</label>
-        <p>. . .</p>
     </div>
+    <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
 
 
@@ -83,6 +88,6 @@
         <input type="checkbox" id="box-22"> <label for="box-22">Task</label>
         <input type="checkbox" id="box-23"> <label for="box-23">Task</label>
         <input type="checkbox" id="box-24"> <label for="box-24">Task</label>
-        <p>. . .</p>
     </div>
+    <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
