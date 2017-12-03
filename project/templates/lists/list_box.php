fb3d775d6d4d348c68cb78f79991a649c3fa7a81
julietafrade97 <up201506530@fe.up.pt>
2017-12-03 19:21:39 +0000

    pop up (faltam so os icons das task)

--- project/templates/lists/list_box.php
+++ project/templates/lists/list_box.php
@@ -1,11 +1,38 @@
 <div id="listPopup" class="overlay">
-	<div class="popup">
-		<h2>Here i am</h2>
-		<a class="close" href="#">×</a>
-		<div class="content">
-			Thanks for pop me out of that button, but now i'm done so you can close this window.
-		</div>
-	</div>
+    <div class="popup">
+        <div id="pop_up_header">
+            <span class="lnr lnr-trash"></span>
+            <a id="pop_up_close" href="#"> <span class="lnr lnr-cross"></span></a>
+        </div>
+        <div id="pop_up_title">
+            <h1>Title</h1>
+            <div id="rectangle"></div>
+        </div>
+        <div id="popup_task_list">
+            <div class="content">
+                <div class="task">
+                    <input type="checkbox" id="box-A">
+                    <label for="box-A">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod </label>
+                </div>
+                <input type="checkbox" id="box-B">
+                <label for="box-B">Task</label>
+                <input type="checkbox" id="box-C">
+                <label for="box-C">Task</label>
+                <input type="checkbox" id="box-D">
+                <label for="box-D">Task</label>
+                <input type="checkbox" id="box-E">
+                <label for="box-E">Task</label>
+                <input type="checkbox" id="box-F">
+                <label for="box-F">Task</label>
+                <input type="checkbox" id="box-G">
+                <label for="box-G">Task</label>                
+            </div>
+        </div>
+        <div id="add_task">
+            <span class="lnr lnr-plus-circle"></span>
+            <p>Add Task</p>
+        </div>
+    </div>
 </div>
 
 <div id="list_box">
@@ -31,10 +58,14 @@
         <h1> Title </h1>
     </div>
     <div id="task_list">
-        <input type="checkbox" id="box-5"> <label for="box-5">Task</label>
-        <input type="checkbox" id="box-6"> <label for="box-6">Task</label>
-        <input type="checkbox" id="box-7"> <label for="box-7">Task</label>
-        <input type="checkbox" id="box-8"> <label for="box-8">Task</label>
+        <input type="checkbox" id="box-5">
+        <label for="box-5">Task</label>
+        <input type="checkbox" id="box-6">
+        <label for="box-6">Task</label>
+        <input type="checkbox" id="box-7">
+        <label for="box-7">Task</label>
+        <input type="checkbox" id="box-8">
+        <label for="box-8">Task</label>
     </div>
     <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
@@ -45,10 +76,14 @@
         <i class="fa fa-bookmark" aria-hidden="true"></i>
     </div>
     <div id="task_list">
-        <input type="checkbox" id="box-9"> <label for="box-9">Task</label>
-        <input type="checkbox" id="box-10"> <label for="box-10">Task</label>
-        <input type="checkbox" id="box-11"> <label for="box-11">Task</label>
-        <input type="checkbox" id="box-12"> <label for="box-12">Task</label>
+        <input type="checkbox" id="box-9">
+        <label for="box-9">Task</label>
+        <input type="checkbox" id="box-10">
+        <label for="box-10">Task</label>
+        <input type="checkbox" id="box-11">
+        <label for="box-11">Task</label>
+        <input type="checkbox" id="box-12">
+        <label for="box-12">Task</label>
     </div>
     <a id="more" href="#listPopup" type="button" >SEE MORE</a>  
 </div>
@@ -59,10 +94,14 @@
         <i class="fa fa-bookmark" aria-hidden="true"></i>
     </div>
     <div id="task_list">
-        <input type="checkbox" id="box-13"> <label for="box-13">Task</label>
-        <input type="checkbox" id="box-14"> <label for="box-14">Task</label>
-        <input type="checkbox" id="box-15"> <label for="box-15">Task</label>
-        <input type="checkbox" id="box-16"> <label for="box-16">Task</label>
+        <input type="checkbox" id="box-13">
+        <label for="box-13">Task</label>
+        <input type="checkbox" id="box-14">
+        <label for="box-14">Task</label>
+        <input type="checkbox" id="box-15">
+        <label for="box-15">Task</label>
+        <input type="checkbox" id="box-16">
+        <label for="box-16">Task</label>
     </div>
     <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
@@ -73,10 +112,14 @@
         <h1> Title </h1>
     </div>
     <div id="task_list">
-        <input type="checkbox" id="box-17"> <label for="box-17">Task</label>
-        <input type="checkbox" id="box-18"> <label for="box-18">Task</label>
-        <input type="checkbox" id="box-19"> <label for="box-19">Task</label>
-        <input type="checkbox" id="box-20"> <label for="box-20">Task</label>
+        <input type="checkbox" id="box-17">
+        <label for="box-17">Task</label>
+        <input type="checkbox" id="box-18">
+        <label for="box-18">Task</label>
+        <input type="checkbox" id="box-19"> 
+        <label for="box-19">Task</label>
+        <input type="checkbox" id="box-20"> 
+        <label for="box-20">Task</label>
     </div>
     <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
@@ -88,10 +131,14 @@
         <i class="fa fa-bookmark" aria-hidden="true"></i>
     </div>
     <div id="task_list">
-        <input type="checkbox" id="box-21"> <label for="box-21">Task</label>
-        <input type="checkbox" id="box-22"> <label for="box-22">Task</label>
-        <input type="checkbox" id="box-23"> <label for="box-23">Task</label>
-        <input type="checkbox" id="box-24"> <label for="box-24">Task</label>
+        <input type="checkbox" id="box-21"> 
+        <label for="box-21">Task</label>
+        <input type="checkbox" id="box-22"> 
+        <label for="box-22">Task</label>
+        <input type="checkbox" id="box-23"> 
+        <label for="box-23">Task</label>
+        <input type="checkbox" id="box-24"> 
+        <label for="box-24">Task</label>
     </div>
     <a id="more" href="#listPopup" type="button" >SEE MORE</a> 
 </div>
