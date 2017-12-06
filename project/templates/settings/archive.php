b1cf8ff6d54ed86d21dec056a91ce372677d87c1
Sofia Silva <up201505628@fe.up.pt>
2017-12-06 23:31:01 +0000

    added button styles in settings

--- project/templates/settings/archive.php
+++ project/templates/settings/archive.php
@@ -6,11 +6,35 @@
                 <h2>Lists</h2>
                 <ul>
                     <li>
-                        <span class="lnr lnr-chevron-right-circle"></span>List 1</li>
+                        <span class="lnr lnr-chevron-right-circle"></span>
+                        <p>List 1</p>
+                        <button>
+                            <span class="lnr lnr-heart-pulse"></span>
+                        </button>
+                        <button>
+                            <span class="lnr lnr-cross"></span>
+                        </button>
+                    </li>
                     <li>
-                        <span class="lnr lnr-chevron-right-circle"></span>List 2</li>
+                        <span class="lnr lnr-chevron-right-circle"></span>
+                        <p>List 2</p>
+                        <button>
+                            <span class="lnr lnr-heart-pulse"></span>
+                        </button>
+                        <button>
+                            <span class="lnr lnr-cross"></span>
+                        </button>
+                    </li>
                     <li>
-                        <span class="lnr lnr-chevron-right-circle"></span>List 3</li>
+                        <span class="lnr lnr-chevron-right-circle"></span>
+                        <p>List 3</p>
+                        <button>
+                            <span class="lnr lnr-heart-pulse"></span>
+                        </button>
+                        <button>
+                            <span class="lnr lnr-cross"></span>
+                        </button>
+                    </li>
                 </ul>
                 <input type="submit" name="Submit" value="Clear">
             </div>
