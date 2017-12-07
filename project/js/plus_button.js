461de4d842245e66c22ae1b28fb17c7ab7f98c26
julietafrade97 <up201506530@fe.up.pt>
2017-12-07 15:31:20 +0000

    adicionar lista no botao

--- project/js/plus_button.js
+++ project/js/plus_button.js
@@ -7,4 +7,13 @@ function plus_button(){
 
 function add_list(){
   /*alert("Add list");*/
+
+  let list_grid = document.getElementById("grid_container");
+
+  let new_list_box = document.createElement('div');
+
+  new_list_box.innerHTML = '<div id="list_box"> <div id="list_box_header"> <h1> New Title </h1> <i class="fa fa-bookmark" aria-hidden="true"></i></div> <div id="task_list"> </div> <a id="more" href="#listPopUp" type="button">SEE MORE</a> </div>'
+
+  list_grid.insertBefore(new_list_box, list_grid.lastChild);
+
 }
