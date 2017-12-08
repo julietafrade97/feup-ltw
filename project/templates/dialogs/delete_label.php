282f85d923a02c6c8d82afc2338da1e1918ede67
Sofia Silva <up201505628@fe.up.pt>
2017-12-08 18:08:44 +0000

    sidebar labels: all new :purse:

---
+++ project/templates/dialogs/delete_label.php
@@ -0,0 +1,9 @@
+<div id="dialogDeleteLabel" class="overlay">
+    <div class="question">
+        <p>Are you sure you want to delete this label?</p>
+        <div class="buttons">
+            <input type="button" value="Cancel">
+            <input type="button" value="Delete">
+        </div>
+    </div>
+</div>
