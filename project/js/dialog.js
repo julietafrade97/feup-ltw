70fcfe7ed15bfdde30a351b9bb946ab88aa10e3c
julietafrade97 <up201506530@fe.up.pt>
2017-12-10 16:25:10 +0000

    little fix

--- project/js/dialog.js
+++ project/js/dialog.js
@@ -50,10 +50,8 @@ function openDialog(value, id) {
       dialog8.style.display = "block";
       break;
     case "List":
-      if(id != null){
         getTasks(id);
         dialog9.style.display = "block";
-      }
       break;
     case "Add Project":
       dialog10.style.display = "block";
