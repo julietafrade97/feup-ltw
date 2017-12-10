35f70a55a00f15cc16ece5b9d3394460df7daef9
Sofia Silva <up201505628@fe.up.pt>
2017-12-10 16:23:07 +0000

    css clean: buttons and span ish

--- project/templates/projects/single_project.php
+++ project/templates/projects/single_project.php
@@ -1,7 +1,9 @@
 <div id="project_container">
     <div id="bio">
         <div class="header">
-            <h1><?php echo $projectBio['Name']?></h1>
+            <h1>
+                <?php echo $projectBio['Name']?>
+            </h1>
             <input type="text">
             <button class="edit">
                 <span class="lnr lnr-pencil"></span>
@@ -10,7 +12,9 @@
                 <span class="lnr lnr-exit"></span>
             </button>
         </div>
-        <p><?php echo $projectBio['Description'] ?></p>
+        <p>
+            <?php echo $projectBio['Description'] ?>
+        </p>
         <textarea></textarea>
     </div>
     <div id="members">
@@ -32,29 +36,23 @@
         <h1>Lists</h1>
         <div id="grid">
             <div id="grid_container">
+
                 <!-- CHANGE -->
                 <div id="list_box">
                     <div id="list_box_header">
-                        <h1> Title </h1>
+                        <h1>Title</h1>
                         <button onclick="openDialog('Change Label')">
-                            <i class="fa fa-bookmark" aria-hidden="true" style="color:#948f8c"></i>
+                            <i class="fa fa-bookmark" aria-hidden="true" style="color: black"></i>
                         </button>
                     </div>
                     <div class="checkboxAndLabel">
-                        <input type="checkbox" id="box-1">
-                        <label for="box-1">Task</label>
-                        <input type="checkbox" id="box-2">
-                        <label for="box-2">Task</label>
-                        <input type="checkbox" id="box-3">
-                        <label for="box-3">Task</label>
-                        <input type="checkbox" id="box-4">
-                        <label for="box-4">Task</label>
                     </div>
-                    <button  id="more" onclick="openDialog('List')">
+                    <button id="more" onclick="openDialog('List')">
                         SEE MORE
                     </button>
                 </div>
                 <!-- CHANGE -->
+
             </div>
         </div>
     </div>
