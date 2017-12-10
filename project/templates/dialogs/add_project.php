cc15c332fc5bf97c2f8f08616ad943aaf97a04e6
Sofia Silva <up201505628@fe.up.pt>
2017-12-10 17:02:17 +0000

    minor fix

--- project/templates/dialogs/add_project.php
+++ project/templates/dialogs/add_project.php
@@ -4,7 +4,7 @@
         <input name="title" class="w3-input w3-border" type="text" placeholder="Title">
         <textarea rows="4" cols="50" placeholder="Description"></textarea>
         <div class="buttons">
-            <input onclick="closeDialog('Add Member')" type="button" value="Cancel">
+            <input onclick="closeDialog('Add Project')" type="button" value="Cancel">
             <input type="submit" value="Submit">
         </div>
     </div>
