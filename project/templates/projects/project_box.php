7c0c49923c2d4c62e105bbe681bfa7233c095ff1
Sofia Silva <up201505628@fe.up.pt>
2017-12-12 12:31:41 +0000

    fixed html

--- project/templates/projects/project_box.php
+++ project/templates/projects/project_box.php
@@ -1,18 +1,19 @@
 <?php foreach($projects as $project) {?>
-    <div class="project_box">
-        <h1><?php echo $project['Name']?></h1>
-        <p><?php echo $project['Description']?></p>
-        <div class="tiny_members">
-            <img class="member" src="../assets/portrait.png" alt="Project Member">
-            <img class="member" src="../assets/portrait.png" alt="Project Member">
-            <img class="member" src="../assets/portrait.png" alt="Project Member">
-            <img class="member" src="../assets/portrait.png" alt="Project Member">
-            <p>...</p>
-        </div>
-        <a href="project.php?project_id=<?php echo $project['ID']?>">
-            <button>
-                <span class="lnr lnr-chevron-right-circle"></span>
-            </button>
-        </a>
+<div class="project_box">
+    <h1>
+        <?php echo $project['Name']?>
+    </h1>
+    <p>
+        <?php echo $project['Description']?>
+    </p>
+    <div class="tiny_members">
+        <img class="member" src="../assets/portrait.png" alt="Project Member">
+        <img class="member" src="../assets/portrait.png" alt="Project Member">
+        <img class="member" src="../assets/portrait.png" alt="Project Member">
+        <img class="member" src="../assets/portrait.png" alt="Project Member">
     </div>
+    <a href="project.php?project_id=<?php echo $project['ID']?>">
+        <span class="lnr lnr-chevron-right-circle"></span>
+    </a>
+</div>
 <?php }?>
