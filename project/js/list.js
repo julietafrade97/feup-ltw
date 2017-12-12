445ecb93166452765c9c9f5070c15be2adc0564b
Sofia Silva <up201505628@fe.up.pt>
2017-12-12 15:21:19 +0000

    minor fix

--- project/js/list.js
+++ project/js/list.js
@@ -1,27 +1,25 @@
-
 function addNewList() {
-    let submit = document.getElementById("pick-label-btn");
-    let label = document.querySelector("input[type=radio][name=radioLabels]:checked");
-    if(label != null) label = label.getAttribute("id");
-    if(label != null) label = label.substr(5);
-    createListAjax(label);
-
-
+  let submit = document.getElementById("pick-label-btn");
+  let label = document.querySelector(
+    "input[type=radio][name=radioLabels]:checked"
+  );
+  if (label != null) label = label.getAttribute("id");
+  if (label != null) label = label.substr(5);
+  createListAjax(label);
 }
 
 function createListAjax(label) {
-    let request = new XMLHttpRequest();
-    request.addEventListener("load", finishCreateList);
-    request.open("post", "../actions/api_add_list.php", true);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.send(encodeForAjax({labelID: label}));
+  let request = new XMLHttpRequest();
+  request.addEventListener("load", finishCreateList);
+  request.open("post", "../actions/api_add_list.php", true);
+  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  request.send(encodeForAjax({ labelID: label }));
 }
 
-
 function finishCreateList(event) {
-    event.preventDefault();
-    let newListID;
-    if((newListID = JSON.parse(this.responseText)) != -1) {
-        openDialog('List', newListID);
-    }
+  event.preventDefault();
+  let newListID;
+  if ((newListID = JSON.parse(this.responseText)) != -1) {
+    openDialog("List", newListID);
+  }
 }
